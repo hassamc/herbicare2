b50fd96b1b9ac483bd8a6b6a8713b7348a073431
hassamc <hassam.6@live.com>
2019-09-02 23:09:55 -0400

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,32 @@ module.exports = {
     `gatsby-plugin-stripe`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146952821-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "GTM-5N2KX8N",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YO_jEM9NElRniWyy2yJfNEeA",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "herbicare.store",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
